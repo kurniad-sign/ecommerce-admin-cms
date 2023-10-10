649ecea3041f6540e5026904ced061a5bf5036b7
kurniagung <agungkurniadi99@gmail.com>
2023-10-10 11:27:45 +0700

    add colors page index and form

---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,34 @@
+import { format } from "date-fns";
+import prismadb from "@/lib/db";
+import { SizesClient } from "./components/client";
+import { ColorsColumn } from "./components/columns";
+
+export default async function ColorsPage({
+  params,
+}: {
+  params: { storeId: string };
+}) {
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedColor: ColorsColumn[] = colors.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizesClient data={formattedColor} />
+      </div>
+    </div>
+  );
+}
