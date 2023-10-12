aef794a722fd066ba91fb98381dd3eed9fb01772
kurniagung <agungkurniadi99@gmail.com>
2023-10-12 18:09:26 +0700

    add route for webhook stripe

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -106,6 +106,9 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
 
     return NextResponse.json(category);
