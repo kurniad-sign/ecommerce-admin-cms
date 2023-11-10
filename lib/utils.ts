186df9bdac44ef434eb2b06bf0068722fa3bb928
kurniagung <agungkurniadi99@gmail.com>
2023-11-10 16:02:39 +0700

    add error boundary for store page

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export const wait = (time: number) =>
+  new Promise((res) => setTimeout(res, time));
+
 export const priceFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
